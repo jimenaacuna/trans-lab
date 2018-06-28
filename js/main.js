06346cd833f320c7b0f53e583dbb56b4e64afe6b
jimenaacuna <jimena.a.saavedra@gmail.com>
2018-06-28 11:52:17 -0400

    funcion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-/* Holiii acá va tu código también */
+
+function dataBip(numberBip) {
+    Promise.all([
+      fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberBip}`)
+    ]).then((responses)=>{
+      return Promise.all(responses.map((response => response.json())));
+    }).then((responseJsons)=>{ 
+      const dataTarjeta = responseJsons[0]
+      console.log(dataTarjeta);
+    });
+    
+  };
